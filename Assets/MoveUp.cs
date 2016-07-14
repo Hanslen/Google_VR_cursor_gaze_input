4d7e75012b05ecd7f547c6a722fe865176955b61
Jiahe Chen <psyjc3@exmail.nottingham.ac.uk>
2016-07-14 11:56:47 +0800

    Google Cardboard's cursor and gaze input
    Google Cardboard's cursor and gaze input

---
+++ Assets/MoveUp.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void MoveBoxUp(){
+		transform.position += new Vector3(0f, 1f, 0f);
+	}
+
+}
